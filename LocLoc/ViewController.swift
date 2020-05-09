719442424ef06e84e60e24734ff6855073d1172d
Noam <noam.bartouv@tg-17.com>
2020-05-09 12:30:56 +0300

    first commit

---
+++ LocLoc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocLoc
+//
+//  Created by Noam Bar-Touv on 09/05/2020.
+//  Copyright © 2020 Noam Bar-Touv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
